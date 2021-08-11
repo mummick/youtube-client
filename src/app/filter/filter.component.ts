44b7c85088f94752c598ad3508674edae336f940
mummick <mummick6@gmail.com>
2021-08-11 18:10:55 +0300

    fix: fix lint errors

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss'],
 })
-export class FilterComponent implements OnInit {
-  ngOnInit(): void {}
-}
+export class FilterComponent {}
